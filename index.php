c08b076c05a12978b3d67d8854e7201d1ed83b44
Ederlino Camargos <ederlinoacamargos05@gmail.com>
2020-07-02 16:55:23 -0300

    Aula 65. PDO - DAO - LIST

--- index.php
+++ index.php
@@ -2,18 +2,33 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-
-$root->loadById(3);
-
-echo $root;
-
-
-
+//====================================================
 // $sql = new Sql();
-
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios ");
-
 // echo json_encode($usuarios);
+
+//==============================================================
+// carrega 1 usuário 
+// $root = new Usuario();
+// $root->loadById(3);
+// echo $root;
+
+//==============================================================
+// // carrega uma lista de usuarios
+// $list = Usuario::getList();
+// echo json_encode($list);
+
+//==============================================================
+// // carrega uma lis de usuários buscando pelo login 
+// $search = Usuario::search("jo");
+// echo json_encode($search);
+
+//==============================================================
+// Carrega um usuário usando o login e a senha 
+$usuario = new Usuario();
+// $usuario->login("root","alter");
+$usuario->login("root","altereee"); // Acionando erro 
+
+echo $usuario;
 
 ?>
